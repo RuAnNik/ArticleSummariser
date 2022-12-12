9427f6fe7697e0def9e688d461549ce78def9376
Frame24 <hks9@yandex.ru>
2022-12-12 18:55:14 +0500

    +client+server

---
+++ Client.py
@@ -0,0 +1,12 @@
+import json
+import streamlit as st
+import requests as rq
+
+article_text = "Впрочем, иногда странам удаётся вести конфликты более мирным путём. Например, Канада и Дания не могут поделить маленький остров Ханс, который вы можете видеть на иллюстрации. Поэтому на острове ведётся Hans Island так называемая «интеллигентная война». Раз в несколько месяцев туда прибывают военно‑морские силы Канады, устанавливают на острове флаг своего государства, поглощают заранее оставленный противником на острове запас крепких напитков, отмечают взятие острова и с победой отбывают."
+article_text = st.text_area(label= "Текст:", value=article_text, )
+data = {'text': article_text}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+st.write("Краткое содержание:")
+summary = rq.get("http://127.0.0.1:8000/predict/", data=json.dumps(data), headers=headers).json()["summary"]
+st.write(summary)
---
+++ Server.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+model_name = "IlyaGusev/rut5_base_sum_gazeta"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = T5ForConditionalGeneration.from_pretrained(model_name)
+
+class Article(BaseModel):
+ text: str
+ 
+@app.get("/predict/")
+async def predict(article: Article):
+    input_ids = tokenizer(
+        [article.text],
+        max_length=600,
+        add_special_tokens=True,
+        padding="max_length",
+        truncation=True,
+        return_tensors="pt"
+    )["input_ids"]
+
+    output_ids = model.generate(
+        input_ids=input_ids,
+        no_repeat_ngram_size=4
+    )[0]
+
+    summary = tokenizer.decode(output_ids, skip_special_tokens=True)
+    return {"summary": summary}
+
+@app.get("/")
+async def root():
+ return {"message": "Hello World"}
